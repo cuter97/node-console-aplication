ad15fc0dc989325781f728be27f836b73b422154
cuter97 <aguscastets@gmail.com>
2023-09-10 20:18:02 -0300

    add testing in server-app.ts and args.plugins.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,7 @@
-describe('App', () => {
-    test('should first', () => {
+describe('Test app.ts', () => {
+
+    test('should call server.run with values', () => {
         expect(true).toBe(true);
     });
+
 });
--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -52,4 +52,34 @@ describe('SaveFileUseCase', () => {
 
     });
 
+    test('should return false if directory could not be created', () => {
+
+        const saveFile = new SaveFile();
+        const mkdirSpy = jest.spyOn(fs, 'mkdirSync').mockImplementation(
+            () => { throw new Error('This is a custom error message from testing'); }
+        );
+
+        const result = saveFile.execute(customOptions);
+
+        expect(result).toBe(false);
+
+        mkdirSpy.mockRestore();
+
+    });
+
+    test('should return false if file could not be created', () => {
+
+        const saveFile = new SaveFile();
+        const writeFileSpy = jest.spyOn(fs, 'writeFileSync').mockImplementation(
+            () => { throw new Error('This is a custom error message from testing'); }
+        );
+
+        const result = saveFile.execute({ fileContent: 'Test' });
+
+        expect(result).toBe(false);
+        
+        writeFileSpy.mockRestore();
+
+    });
+
 });
